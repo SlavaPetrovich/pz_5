2cf5385f9cfe3b55fe32d4ce391c9dd68c02cce9
Slava <petrovichvecheslav@yandex.ru>
2022-10-23 21:09:54 +0300

    lesson_2

---
+++ lesson_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Random a = new Random();
+int b = a.Next(-99, 100);
+int c = a.Next(-99, 100);
+int d = a.Next(-99, 100);
+int e = a.Next(-99, 100);
+int[] x = {b, c, d, e};
+int counter = 0;
+
+for (int i = 0; i < 4; i++)
+{
+    if (i % 2 != 0)
+    {
+        counter = counter + x[i];
+    }
+}
+
+Console.WriteLine($"[{b}, {c}, {d}, {e}] -> {counter}");
