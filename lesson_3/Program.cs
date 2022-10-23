34c5199002bc51a0c5f69049f4372449a78d3433
Slava <petrovichvecheslav@yandex.ru>
2022-10-23 21:34:50 +0300

    lesson_3

---
+++ lesson_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Random a = new Random();
+int b = a.Next(1, 100);
+int c = a.Next(1, 100);
+int d = a.Next(1, 100);
+int e = a.Next(1, 100);
+int[] x = {b, c, d, e};
+
+int max = x.Max();
+int min = x.Min();
+
+int answer = max - min;
+Console.WriteLine($"[{b}, {c}, {d}, {e}] -> {answer}");
+
